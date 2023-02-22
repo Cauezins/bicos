5908b15246ee08bf56a6989ef24eb3275339e226
Cauezins <109925592+Cauezins@users.noreply.github.com>
2023-02-21 23:52:33 -0300

    Add files via upload

--- confi.js
+++ confi.js
@@ -1,13 +1,65 @@
+function slider() {
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        freeMode: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        breakpoints:{
+            0: {
+                slidesPerView: 1,
+            },
+            620: {
+                slidesPerView: 2,
+            },
+            1000: {
+                slidesPerView: 3,
+            },
+        }
+      });
+}
+
 function searchColor() {
-    document.getElementById('searchButton').style.backgroundColor = '#f5f5f5'
-    document.getElementById('search').style.backgroundColor = '#f5f5f5'
+    if(document.getElementsByTagName('link')[0].getAttribute('href') == 'style.css'){
+        document.getElementById('searchButton').style.backgroundColor = '#f5f5f5'
+        document.getElementById('search').style.backgroundColor = '#f5f5f5'
+    } else{
+        document.getElementById('searchButton').style.backgroundColor = '#535353'
+        document.getElementById('search').style.backgroundColor = '#535353'
+    }
 }
 function searchColorNorm() {
-    document.getElementById('searchButton').style.backgroundColor = 'white'
-    document.getElementById('search').style.backgroundColor = 'white'
+    if(document.getElementsByTagName('link')[0].getAttribute('href') == 'style.css'){
+        document.getElementById('searchButton').style.backgroundColor = 'white'
+        document.getElementById('search').style.backgroundColor = 'white'
+    } else{
+        document.getElementById('searchButton').style.backgroundColor = '#373737'
+        document.getElementById('search').style.backgroundColor = '#373737'
+    }
+    
 }
 function year() {
     const dataAtual = new Date();
     let anoAtual = dataAtual. getFullYear();
     document.getElementById('dateCopy').innerHTML = anoAtual
+}
+function dark() {
+    var theme = document.getElementsByTagName('link')[0];
+    if (theme.getAttribute('href') == 'style.css') {
+        theme.setAttribute('href', 'dark.css')
+        document.getElementById('light').id = 'dark'
+        document.getElementById('imgD').setAttribute('src', 'sun.svg')
+        document.getElementById('img').setAttribute('src', 'searchlight.svg')
+        document.getElementById('searchButton').style.backgroundColor = '#373737'
+        document.getElementById('search').style.backgroundColor = '#373737'
+    } else {
+        theme.setAttribute('href', 'style.css')
+        document.getElementById('dark').id = 'light'
+        document.getElementById('imgD').setAttribute('src', 'moon.svg')
+        document.getElementById('img').setAttribute('src', 'search.svg')
+        document.getElementById('searchButton').style.backgroundColor = 'white'
+        document.getElementById('search').style.backgroundColor = 'white'
+    }
 }
