a99bff681fdb870e2a9af24cd0e407fba380f891
KauanAg-devs <117299925+KauanAg-devs@users.noreply.github.com>
2023-02-22 23:15:17 -0300

    Update confi.js

--- confi.js
+++ confi.js
@@ -90,3 +90,96 @@ function year() {
     let anoAtual = dataAtual. getFullYear();
     document.getElementById('dateCopy').innerHTML = anoAtual
 }
+
+// FUNÇÃO LOGIN
+var opacidade = 1
+function signUpFunction() {
+
+   var form = document.createElement('FORM')
+   form.setAttribute('id', 'signUpForm');
+    var name = document.createElement('INPUT')
+    name.setAttribute('class', 'signUpInput')
+    name.setAttribute('type', 'text')
+    name.setAttribute('placeholder', 'Nome ');
+
+    var lastName = document.createElement('INPUT')
+    lastName.setAttribute('class', 'signUpInput')
+    lastName.setAttribute('type', 'text')
+    lastName.setAttribute('placeholder', 'Sobrenome ');
+
+    var cpf = document.createElement('INPUT')
+    cpf.setAttribute('class', 'signUpInput')
+    cpf.setAttribute('type', 'text')
+    cpf.setAttribute('placeholder', 'CPF ');
+
+    var cnpj = document.createElement('INPUT')
+    cnpj.setAttribute('class', 'signUpInput')
+    cnpj.setAttribute('type', 'text')
+    cnpj.setAttribute('placeholder', 'CNPJ ');
+
+    var email = document.createElement('INPUT')
+    email.setAttribute('class', 'signUpInput')
+    email.setAttribute('type', 'text')
+    email.setAttribute('placeholder', 'Email ');
+
+    var birthday = document.createElement('INPUT')
+    birthday.setAttribute('class', 'signUpInput')
+    birthday.setAttribute('type', 'text')
+    birthday.setAttribute('placeholder', 'Data de Nascimento ');
+
+    var password = document.createElement('INPUT')
+    password.setAttribute('class', 'signUpInput')
+    password.setAttribute('type', 'text')
+    password.setAttribute('placeholder', 'Senha ');
+    
+    var button = document.createElement('INPUT')
+    button.setAttribute('class', 'signUpInput')
+    button.setAttribute('id', 'signUpButton')
+    button.setAttribute('type', 'button')
+    button.setAttribute('value', 'Cadastrar ');
+    
+    var h1 = document.createElement('H1')
+    h1.setAttribute('id', 'h1')
+    h1.setAttribute("onclick","eraseForm()");
+    h1.innerHTML = 'X'; h1.style.fontSize = '20px'; 
+    h1.style.position = 'relative';
+    h1.style.left = '120px';
+
+
+
+    document.getElementById('anuncioCad').appendChild(form).appendChild(h1)
+    document.getElementById('signUpForm').appendChild(name)
+    document.getElementById('signUpForm').appendChild(lastName)
+    document.getElementById('signUpForm').appendChild(cpf)
+    document.getElementById('signUpForm').appendChild(cnpj)
+    document.getElementById('signUpForm').appendChild(email)
+    document.getElementById('signUpForm').appendChild(birthday)
+    document.getElementById('signUpForm').appendChild(password)
+    document.getElementById('signUpForm').appendChild(button)
+    
+   
+    if(opacidade == 1)     document.getElementById('anuncioButton').onclick = null;
+       
+  
+
+      }
+    //   FIM DA FUNÇÃO LOGIN
+    
+   
+        function eraseForm () {
+        var intervalo = 1500 / 10;
+        var timer = setInterval(function() {
+        document.getElementById('signUpForm').style.opacity = opacidade -= 0.1;
+        if(opacidade <= 0) {
+        clearInterval(timer);
+        document.getElementById('signUpForm').style.display = 'none';
+        document.getElementById('signUpForm').remove()
+        document.getElementById('anuncioButton').onclick = signUpFunction
+        opacidade = 1
+        }
+        }, intervalo); 
+      
+       
+        
+       }
+        
