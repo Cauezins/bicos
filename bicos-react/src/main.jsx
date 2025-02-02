4bebd5019ace1f84f06ca13f36adde1095278b60
Cauezins <caue.m0000@gmail.com>
2025-02-02 16:02:42 -0300

    fix: add routes with react-router

--- bicos-react/src/main.jsx
+++ bicos-react/src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import HomePage from './views/HomePage'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
   </StrictMode>,
 )
---
+++ bicos-react/src/views/HomePage.jsx
@@ -0,0 +1,8 @@
+import NavBar from '../components/NavBar';
+function HomePage() {
+  return (
+   <NavBar />
+  );
+}
+
+export default HomePage;
